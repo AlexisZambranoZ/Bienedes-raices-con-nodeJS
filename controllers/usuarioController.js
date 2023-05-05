49844bdbe4c96a799b4726dc695e5bd8e3c5fcf0
Alexis <zambra873@gmail.com>
2023-05-04 19:06:46 -0500

    formularios

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,16 +1,23 @@
 const formularioLogin = (req, resp) => {
     resp.render('auth/login', {
-     
+        pagina: "Iniciar Sesion"
     })
 }
 
 const formularioRegister = (req, resp) => {
     resp.render('auth/register', {
-     
+        pagina: "Crear Cuenta"
+    })
+}
+
+const formularioForgetPassword = (req, resp) => {
+    resp.render('auth/olvide-password', {
+        pagina: "Recupera tu acceso a bienes raices"
     })
 }
 
 export {
     formularioLogin,
-    formularioRegister
+    formularioRegister,
+    formularioForgetPassword
 }
